3dde60e7d72853f5115e68a2cb1480a902f1d7db
cccc4444 <kushlyansky244@gmail.com>
2022-08-05 20:57:50 +0300

    Api Key removed

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,43 @@
 
 import Foundation
 
+
 struct CoinManager {
     
+    var delegate: CoinManagerDelegate?
+    
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let fiat = "USD"
+    let apiKey = ""
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
+    
+    func performRequest(fiat: String){
+        if let url = URL(string: "\(baseURL)/\(fiat)?apikey=\(apiKey)"){
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil{
+                    delegate?.didGetError(error!)
+                    return
+                }
+                
+                guard let data = data else {
+                    return
+                }
+                
+                do {
+                    let parsedData = try JSONDecoder().decode(Coin.self, from: data)
+                    let coinData = Coin(rate: parsedData.rate)
+                    delegate?.didUpdatePrice(coinData)
+                
+                } catch  {
+                    delegate?.didGetError(error)
+                }
+                
+                
+            }
+            task.resume()
+        }
+    }
     
 }
